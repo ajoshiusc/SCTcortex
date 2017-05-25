b5a4b3dba38eaad508aedb6cf1bddc8d48144c72
suyash <suyash@cs.utah.edu>
2017-05-25 11:21:32 +0530

    first reorganization

--- src_new/covHypersphere.m
+++
@@ -1,9 +0,0 @@
-function [ cov t] = covHypersphere (mu, p)
-
-% p is a matrix: (dimension, numOfPoints)
-% mu is the frechet mean: (dimension, 1)
-
-t = logMapHypersphere (mu, p);
-cov = (t * t') / size (t, 2);
-
-return
--- src_new/sub_SCT_modified.m
+++
@@ -1,8 +0,0 @@
-function sub_SCT(subname)
-pth=fileparts(subname);
-
-[SCTleft,SCTatlasleft]=cortical_SCT_measures_modified([subname,'.left.mid.cortex.svreg.dfs'],[pth,'/atlas.left.mid.cortex.svreg.dfs']);
-[SCTright,SCTatlasright]=cortical_SCT_measures_modified([subname,'.right.mid.cortex.svreg.dfs'],[pth,'/atlas.right.mid.cortex.svreg.dfs']);
-
-save([subname,'.SCT.mat'],'SCTleft','SCTright','SCTatlasleft','SCTatlasright');
-
